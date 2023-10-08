d69053e6a0bc7d09c5abd3d9eee775602b93a04d
Henil Soni <henil.soni@accenture.com>
2023-10-08 22:48:03 +0100

    Initial Test Commit

---
+++ src/test/java/com/capstone/capstone/service/TextEvidenceServiceTest.java
@@ -0,0 +1,46 @@
+package com.capstone.capstone.service;
+
+import com.capstone.capstone.model.Evidence;
+import com.capstone.capstone.repository.EvidenceRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
+class TextEvidenceServiceTest {
+
+    @Mock
+    @Autowired
+    private EvidenceRepository evidenceRepository;
+
+    @InjectMocks TextEvidenceService textEvidenceService;
+
+    @Test
+    void checkFindAllMethod() {
+
+        when(evidenceRepository.findAll())
+                .thenReturn(List.of(Evidence.builder()
+                        .id("123")
+                        .evidence("evidence")
+                        .KsbCode("s1")
+                        .build()));
+
+        List<Evidence> evidences = textEvidenceService.getAllEvidence();
+
+        verify(evidenceRepository, times(1)).findAll();
+
+
+
+
+
+    }
+}
